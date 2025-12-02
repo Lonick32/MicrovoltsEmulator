87002b042e92c4dfe166ff908986a768a0147a1f
SoWeBegin <akames19988@gmail.com>
2025-12-02 09:49:48 -0800

    fix item durability damage implicit conversion error

--- MainServer/src/Classes/Player.cpp
+++ MainServer/src/Classes/Player.cpp
@@ -687,7 +687,7 @@ namespace Main
 
 				item.durability = newDurability;
 				damages.push_back(Main::ClientData::SingleWeaponDurabilityDamage{ item.serialInfo, reduction });
-				idsAndItemNumbers.push_back(std::pair{ static_cast<std::uint32_t>(item.id), item.serialInfo.itemNumber });
+				idsAndItemNumbers.push_back(std::pair{ static_cast<std::uint32_t>(item.id), static_cast<std::uint64_t>(item.serialInfo.itemNumber) });
 			}
 
 			return std::pair{ damages, idsAndItemNumbers };
